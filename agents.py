63d20157c1a8a2397a3665fb1e272f70c41a672b
PriyanshuXsingh <162482010+PriyanshuXsingh@users.noreply.github.com>
2026-09-09 19:28:06 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 #Model Setup
 llm = ChatGroq(
-    model="llama-3.1-8b-instant",
+    model="openai/gpt-oss-20b",
     groq_api_key=os.getenv("GROQ_API_KEY"),
     temperature=0
 )
